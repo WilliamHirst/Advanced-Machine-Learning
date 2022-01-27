f61b6509e4ccccda537848e06701462bf65673fe
Gadangadang <sakarias.frette@outlook.com>
2022-01-27 11:49:51 +0100

    Added readability

--- Codebase/SupervisedSolver.py
+++ Codebase/SupervisedSolver.py
@@ -2,8 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class SupervisedSolver:
-    def __init__():
+    def __init__(self, datafile):
+        self.datafile = datafile
 
+        file = open(self.datafile, 'r')
+        print(len(file.readline().split(",")))
 
 
 if __name__ == "__main__":
+    ss = SupervisedSolver("../Data/training.csv")
