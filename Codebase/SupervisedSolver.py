a33f0fa6bca2ef51010dd856a9b16f920c9ca790
William Hirst <bod_holthe@outlook.com>
2022-01-27 11:50:01 +0100

    Added tf

--- Codebase/SupervisedSolver.py
+++ Codebase/SupervisedSolver.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import optimizers, regularizers
+from tensorflow.keras.layers import Dense, Input
 
 class SupervisedSolver:
     def __init__():
