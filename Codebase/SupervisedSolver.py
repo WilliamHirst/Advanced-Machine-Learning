9d38c3c1e3ea7eb2a12a2b1f668e9cac79281182
Gadangadang <sakarias.frette@outlook.com>
2022-01-27 12:08:55 +0100

    Added panda dataframe to code

--- Codebase/SupervisedSolver.py
+++ Codebase/SupervisedSolver.py
@@ -3,19 +3,17 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import optimizers, regularizers
 from tensorflow.keras.layers import Dense, Input
-
+import pandas as pd 
+    
+ 
 class SupervisedSolver:
     def __init__(self, datafile):
         self.datafile = datafile
-        self.data = self.readFile(self.datafile)
-
-
-    def readFile(self, datafile):
-        file = open(datafile, 'r')
-        self.number_of_categories = len(file.readline().split(","))
-        print(self.number_of_categories)
+        self.dataframe = pd.read_csv(self.datafile)
+        self.length_frame = len(self.dataframe)
         
-        return 0
+    
 
 if __name__ == "__main__":
     ss = SupervisedSolver("../Data/training.csv")
+    print(ss.length_frame)
