42bed8fa898661d6e8a116b3b586e72875a07d81
Gadangadang <sakarias.frette@outlook.com>
2022-02-06 15:03:07 +0100

    Added class for datasets

---
+++ Supervised project/Codebase/dataset.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Dataset:
+    def __init__(self, dataset_train, dataset_test): 
+        self.data_train = dataset_train
+        self.data_test = dataset_test
+        
+        # Make pandas dataframe
+        
+        self.train_df = pd.DataFrame(self.data_train)
+        self.test_df = pd.DataFrame(self.data_test)
+        
+        
+    def __call__(self):
+        return 0
+    
+    
+    
+if __name__ == "__main__":
+    featuresTrain = np.load("../Data/featuresTrain.npy")
+    targetsTrain = np.load("../Data/targetsTrain.npy")
+    
+    data_obj = Dataset(featuresTrain, targetsTrain)
+    
+    
