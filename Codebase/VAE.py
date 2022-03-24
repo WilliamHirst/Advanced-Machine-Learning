4849eb87336fd1961af98bc72b0c0e157bd9b2b3
William Hirst <bod_holthe@outlook.com>
2022-03-24 18:01:17 +0100

    Removed uneeded variables.

--- Codebase/VAE.py
+++ Codebase/VAE.py
@@ -89,6 +89,7 @@ def compute_loss(model, x):
     return loss
 
 
+
 @tf.function
 def train_step(model, x, optimizer):
     with tf.GradientTape() as tape:
@@ -119,11 +120,6 @@ def create_batched_data(X_train, y_train, X_val, y_val, batch_size):
 def train_val_VAE(train_dataset, validation_dataset, batchsize, epochs=10, latentdim=5):
 
     # Create batches
-
-    epochs = epochs
-    latentdim = latentdim
-    batchsize = batchsize
-
     optimizer = tf.keras.optimizers.Adam()
 
     vae = VAE(latentdim, batchsize)  # .compile(optimizer=optimizer)
@@ -143,9 +139,9 @@ def train_val_VAE(train_dataset, validation_dataset, batchsize, epochs=10, laten
             for val_x in validation_dataset:
                 loss(compute_loss(vae, val_x))
 
-            elbo = -loss.result()
+            elbo = -loss.result() 
             print(
-                "Epoch: {}, Test set ELBO: {}, time elapse for current epoch: {}".format(
+                "Epoch: {}, Test set ELBO: {:.2e}, time elapse for current epoch: {:.2e}s".format(
                     epoch, elbo, end_time - start_time
                 )
             )
